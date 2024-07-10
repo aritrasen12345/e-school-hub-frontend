5bbc01da0f6444f9543fca843e8be28e15f28c46
Aritra Sen <aritrasen12345@gmail.com>
2024-07-10 08:48:44 +0530

    added modal components

---
+++ src/components/modal/AuthModal.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { Modal } from "antd";
+import Signup from "../Signup";
+import Login from "../Login";
+
+const AuthModal = ({ open, onCancel }) => {
+  const [modalValue, setModalValue] = useState("");
+
+  const singnupHandler = (value) => {
+    setModalValue(value);
+  };
+
+  const loginHandler = (value) => {
+    setModalValue(value);
+  };
+
+  return (
+    <Modal
+      style={{ textAlign: "center" }}
+      title={
+        modalValue === "" || modalValue === "signup"
+          ? "Please Sign Up"
+          : "Welcome back, Please login"
+      }
+      open={open}
+      onCancel={onCancel}
+      footer={null}
+    >
+      {modalValue === "" || modalValue === "signup" ? (
+        <Signup valueFromSignup={singnupHandler} />
+      ) : (
+        <Login valueFromLogin={loginHandler} />
+      )}
+    </Modal>
+  );
+};
+
+export default AuthModal;
