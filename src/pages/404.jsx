b4b453b807e46dfa4fb871d496eea69862e98366
Aritra Sen <aritrasen12345@gmail.com>
2024-07-10 08:55:38 +0530

    added pagenotfound page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { Button, Result } from "antd";
+import { useNavigate } from "react-router-dom";
 
 const PageNotFound = () => {
-  return <div>404</div>;
+  const navigate = useNavigate();
+
+  const backButtonHandler = (e) => {
+    e.preventDefault();
+    navigate("/");
+  };
+
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Button onClick={backButtonHandler} type="primary">
+          Back Home
+        </Button>
+      }
+    />
+  );
 };
 
 export default PageNotFound;
